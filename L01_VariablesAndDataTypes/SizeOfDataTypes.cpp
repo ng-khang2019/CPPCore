b56f6531fad8bd44f34e0e73f9217b57ae220af2
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-26 14:36:10 +0700

    Size of each data type

---
+++ L01_VariablesAndDataTypes/SizeOfDataTypes.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // Char type
+    cout << left << setw(25) << "Char Type" << "Size in Bytes" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << setw(25) << "char" << sizeof(char) << endl;
+    cout << setw(25) << "char16_t" << sizeof(char16_t) << endl;
+    cout << setw(25) << "char32_t" << sizeof(char32_t) << endl;
+    cout << setw(25) << "wchar_t" << sizeof(wchar_t) << endl;
+    cout << endl;
+
+    // Integer type
+    cout << left << setw(25) << "Integer Type" << "Size in Bytes" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << setw(25) << "short int" << sizeof(short int) << endl;
+    cout << setw(25) << "int" << sizeof(int) << endl;
+    cout << setw(25) << "long int" << sizeof(long int) << endl;
+    cout << setw(25) << "long long int" << sizeof(long long int) << endl;
+    cout << endl;
+
+    // Float type
+    cout << left << setw(25) << "Float Type" << "Size in Bytes" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << setw(25) << "float" << sizeof(float) << endl;
+    cout << setw(25) << "double" << sizeof(double) << endl;
+    cout << setw(25) << "long double" << sizeof(long double) << endl;
+    cout <<  endl;
+
+    // Boolean type
+    cout << left << setw(25) << "Boolean Type" << "Size in Bytes" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << setw(25) << "boolean" << sizeof(bool) << endl;
+    cout << endl;
+
+    // String type
+    cout << left << setw(25) << "String Type" << "Size in Bytes" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << setw(25) << "string" << sizeof(string) << endl;
+
+    return 0;
+}
