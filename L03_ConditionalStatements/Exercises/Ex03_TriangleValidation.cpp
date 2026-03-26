fd80b9ca55f395adb67de00c7c4b3b165a6e2102
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-26 16:42:06 +0700

    Complete triangle validity check

--- L03_ConditionalStatements/Exercises/Ex03_TriangleValidation.cpp
+++ L03_ConditionalStatements/Exercises/Ex03_TriangleValidation.cpp
@@ -1,3 +1,27 @@
-//
-// Created by khang-nguyen on 3/25/26.
-//
+#include <iostream>
+using namespace std;
+
+bool IsValidTriangle(float a, float b, float c);
+
+int main() {
+    float a, b, c;
+    cout<<"Enter the length of 3 sides: "<<endl;
+    cin>>a>>b>>c;
+    if (IsValidTriangle(a, b, c))
+    {
+        cout<<"The triangle is valid"<<endl;
+    } else
+    {
+        cout<<"The triangle is not valid"<<endl;
+    }
+}
+
+bool IsValidTriangle(float a, float b, float c) {
+    if (a<0 || b<0 || c<0) {
+        cout<<"Side values must be positive"<<endl;
+        return false;
+    }
+    if ((a + b > c) && (b + c > a) && (a + c > b)) return true;
+    return false;
+}
+
