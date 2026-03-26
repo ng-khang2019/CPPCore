d48241f7b816af0ac4888283beb2893261601c06
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-26 13:45:09 +0700

    Complete 'How to declare constants' in C++

---
+++ L01_VariablesAndDataTypes/ConstantsDeclaration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const float PI = 3.1415926535f;
+const string MY_NAME = "Khang";
+constexpr int SECONDS_PER_DAY = 24 * 60 * 60;
+
+int main ()
+{
+    std::cout<<"Circle area with radius of 5 is: "<<5*5*PI<<endl;
+    std::cout<<"My name is "<<MY_NAME<<endl;
+    std::cout<<"Total seconds in one day are: "<<SECONDS_PER_DAY<< "(s)"<<endl;
+}
