f0ce6646c45bd0d4a35cebbe050670ac3ab4a412
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-26 19:31:13 +0700

    Triangle area

--- L02_Operators/Exercises/Ex01_TriangleArea.cpp
+++ L02_Operators/Exercises/Ex01_TriangleArea.cpp
@@ -1,3 +1,29 @@
-//
-// Created by khang-nguyen on 3/25/26.
-//
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isValidTriangle(float a, float b, float c) {
+    if (a<0 || b<0 || c<0) {
+        cout<<"Side values must be positive"<<endl;
+        return false;
+    }
+    if ((a + b > c) && (b + c > a) && (a + c > b)) return true;
+    return false;
+}
+
+float triangleArea(float a, float b, float c) {
+    float halfPerimeter = (a + b + c) / 2;
+    return sqrt((halfPerimeter-a)*(halfPerimeter-b)*(halfPerimeter-c));
+}
+
+int main() {
+    float a, b, c;
+    cout<<"Enter sides of the triangle: ";
+    cin>>a>>b>>c;
+    if (isValidTriangle(a,b,c)) {
+        cout<<"The triangle is valid"<<endl;
+    }
+    cout<<"Triangle area is "<<triangleArea(a,b,c) <<endl;
+    return 0;
+}
