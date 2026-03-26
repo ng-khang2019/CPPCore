955e9cb711542218493cea15216cfe38d5a2f19b
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-03-26 14:51:32 +0700

    Add unsigned integer type

--- L01_VariablesAndDataTypes/SizeOfDataTypes.cpp
+++ L01_VariablesAndDataTypes/SizeOfDataTypes.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     // Char type
-    cout << left << setw(25) << "Char Type" << "Size in Bytes" << endl;
+    cout << left << setw(25) << "Char type" << "Size in bytes" << endl;
     cout << "-----------------------------------------" << endl;
     cout << setw(25) << "char" << sizeof(char) << endl;
     cout << setw(25) << "char16_t" << sizeof(char16_t) << endl;
@@ -14,7 +14,7 @@ int main()
     cout << endl;
 
     // Integer type
-    cout << left << setw(25) << "Integer Type" << "Size in Bytes" << endl;
+    cout << left << setw(25) << "Integer type" << "Size in bytes" << endl;
     cout << "-----------------------------------------" << endl;
     cout << setw(25) << "short int" << sizeof(short int) << endl;
     cout << setw(25) << "int" << sizeof(int) << endl;
@@ -22,8 +22,17 @@ int main()
     cout << setw(25) << "long long int" << sizeof(long long int) << endl;
     cout << endl;
 
+    // Unsigned Integer type
+    cout << left << setw(25) << "Unsigned integer type" << "Size in bytes" << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << setw(25) << "unsigned short int" << sizeof(short int) << endl;
+    cout << setw(25) << "unsigned int" << sizeof(int) << endl;
+    cout << setw(25) << "unsigned long int" << sizeof(long int) << endl;
+    cout << setw(25) << "unsigned long long int" << sizeof(long long int) << endl;
+    cout << endl;
+
     // Float type
-    cout << left << setw(25) << "Float Type" << "Size in Bytes" << endl;
+    cout << left << setw(25) << "Float type" << "Size in bytes" << endl;
     cout << "-----------------------------------------" << endl;
     cout << setw(25) << "float" << sizeof(float) << endl;
     cout << setw(25) << "double" << sizeof(double) << endl;
@@ -31,13 +40,13 @@ int main()
     cout <<  endl;
 
     // Boolean type
-    cout << left << setw(25) << "Boolean Type" << "Size in Bytes" << endl;
+    cout << left << setw(25) << "Boolean type" << "Size in bytes" << endl;
     cout << "-----------------------------------------" << endl;
     cout << setw(25) << "boolean" << sizeof(bool) << endl;
     cout << endl;
 
     // String type
-    cout << left << setw(25) << "String Type" << "Size in Bytes" << endl;
+    cout << left << setw(25) << "String type" << "Size in bytes" << endl;
     cout << "-----------------------------------------" << endl;
     cout << setw(25) << "string" << sizeof(string) << endl;
 
