7c48326c3bb7217bdb74a8678fbc63825ae3f258
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-07 22:10:40 +0700

    Do some examples

--- Draft.cpp
+++ Draft.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+    enum loai {
+        XUATSAC = 9, GIOI = 8, KHA = 6, DAT = 5
+    };
+    int main ()
+    {
+        float diem; cout<<"Nhập điểm từ (0-10): ";
+        cin>>diem; cout<<"Học lực của bạn thuộc loại ";
+        if (diem >= XUATSAC) cout<<"Xuất sắc"<<endl;
+        else if (diem >= GIOI) cout<<"Giỏi"<<endl;
+        else if (diem >= KHA) cout<<"Khá"<<endl;
+        else if (diem >= DAT) cout<<"Đạt"<<endl;
+        else cout<<"CHƯA ĐẠT"<<endl;
+        return 0;
+    }
 
-}
