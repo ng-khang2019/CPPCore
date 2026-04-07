b4fa650f116c28c37a89aed967cbaf97508b41d1
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-07 22:10:51 +0700

    Add more define example

--- L01_VariablesAndDataTypes/Define.cpp
+++ L01_VariablesAndDataTypes/Define.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #define MAX_BUFFER_SIZE 1024
-#define SQUARE_AREA(x) ((x)*(x))
+#define SQUARE_AREA(x) (x*x)
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define LOOP(i,a,n) for(int i=a;i<n;i++)
 #define DEBUG_MODE
+long int powerOf2(int n);
 
 using namespace std;
 
@@ -16,8 +18,17 @@ int main()
     int max = MAX(20,5);
     cout<< "Max number between 20 and 5 is " << max << endl;
 
+    int n = 15;
+    cout<<"Two to the power of "<<n<<" is: "<<powerOf2(n)<<endl;
+
     #ifdef DEBUG_MODE
         cout <<"Running in debug mode"<< endl;
     #endif
     return 0;
+}
+
+long int powerOf2(int n) {
+    long int result = 1;
+    LOOP(i,0,n) {result *= 2;};
+    return result;
 }
