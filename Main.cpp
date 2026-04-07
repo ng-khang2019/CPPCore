b1df8d3bbe2367ce294175b3b3abcd73aa107639
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-07 11:10:49 +0700

    Update standard C++ program example

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,23 @@
-#include <iostream>
-using namespace std;
-int main ()
-{
-    string name;
-    std::cout<<"Your first C++ program is running!"<< endl;
-    std::cout<<"Type your name here: ";
-    std::cin>>name;
-    std::cout<<"Hello "<<name<<"! Let's write some programs";
-    return 0;
-}
+    #include <iostream>
+    #include <string>
+
+    int main ()
+    {
+        std::string name;
+        std::cout<<"Your first C++ program is running!"<< std::endl;
+        std::cout<<"Type your name here: ";
+        std::cin>>name;
+        std::cout<<"Hello "<<name<<"! Let's write some programs";
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
