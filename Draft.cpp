7614187f681d20c70fdbb2f26d3cb78705a695f8
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-08 17:48:26 +0700

    Fix indentations

--- Draft.cpp
+++ Draft.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-    int main ()
-    {
-        char ten[10]; int maMon; char tenMon[20]; float diem;
-        cout<<"Nhập tên: ";
-        cin>>ten;
-        cout<<"Nhập tên môn học: ";
-        cin>>tenMon;
+int main()
+{
+    char ten[10];
+    int maMon;
+    char tenMon[20];
+    float diem;
+    cout << "Nhập tên: ";
+    cin >> ten;
+    cout << "Nhập tên môn học: ";
+    cin >> tenMon;
 
-        cout<<"Nhập mã môn học và điểm thi\ntheo định dạng aaa-x.x: ";
-        scanf("%d-%f",&maMon,&diem);
-        printf("%s %03d %s %.1f",ten,maMon,tenMon,diem);
+    cout << "Nhập mã môn học và điểm thi\ntheo định dạng aaa-x.x: ";
+    scanf("%d-%f", &maMon, &diem);
+    printf("%s %03d %s %.1f", ten, maMon, tenMon, diem);
 
-        return 0;
-    }
-
+    return 0;
+}
