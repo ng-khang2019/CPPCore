91e59c35f01a05dcb290db1f3931a731df2391fa
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-16 19:28:55 +0700

    Even or Odd

--- L03_ConditionalStatements/Exercises/Ex02_EvenOdd.cpp
+++ L03_ConditionalStatements/Exercises/Ex02_EvenOdd.cpp
@@ -1,3 +1,14 @@
-//
-// Created by khang-nguyen on 3/25/26.
-//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter a number: ";
+    int n;
+    cin>>n;
+    if (n%2==0) {
+        cout<<"Even";
+    } else {
+        cout<<"Odd";
+    }
+}
