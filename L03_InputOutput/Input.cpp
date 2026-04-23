4b15c019f1439daf8dd174fa24ae16fe6c169d72
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-23 15:45:28 +0700

    Add more input examples

--- L03_InputOutput/Input.cpp
+++ L03_InputOutput/Input.cpp
@@ -6,22 +6,41 @@ using namespace std;
 
 int main()
 {
-    string id;
-    char* phoneNumber{15};
-    string myName;
-
     // cin and scanf() are used for inputting non-space string (single words)
-    cout<<"Enter Student ID: ";
+    // Use scanf() when you need to input string followed a format
+    // Note that scanf() can only be used with char array
+    string id;
+    char phoneNumber[15];
+    char unitCode[4];
+    char roomCode[5];
+
+    cout<<"Enter student ID: ";
     cin>>id;
-    cout<<"Enter Phone Number: ";
-    scanf("%s", phoneNumber);
+    cout<<"Enter phone Number: ";
+    scanf("%s", &phoneNumber);
+    cout<<"Enter unit code and room code following this format ABC-XXXX: ";
+    scanf("%s-%s", &unitCode,&roomCode);
 
     // getline(cin,variable) is used to input space included string (mutiples words)
     // Remember to use cin.ignore() to skip the "\n" after the usage scanf() or cin otherwise
     // getline() will take that "\n" character as an input
+    string myName;
     cin.ignore();
     cout<<"Enter your full name: ";
     getline(cin, myName);
+
+    // cin.getline(variable,size) is used to specify the length of input string
+    // Note that getline() and cin.getline() only work with String class
+    cout<<"Enter your tutor Name: ";
+    char tutorName[50];
+    cin.getline(tutorName,40);
+
+    cout<<"Student ID: "<<id<<endl;
+    cout<<"Phone Number: "<<phoneNumber<<endl;
+    cout<<"Unit Code: "<<unitCode<<endl;
+    cout<<"Room Code: "<<roomCode<<endl;
+    cout<<"Full Name: "<<myName<<endl;
+    cout<<"Tutor Name: "<<tutorName<<endl;
 
     return 0;
 }
