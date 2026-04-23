791332d351762a1499e5849ed4c2bf4ea05de6b5
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-23 15:51:31 +0700

    Clear draft

--- Draft.cpp
+++ Draft.cpp
@@ -3,18 +3,5 @@ using namespace std;
 
 int main()
 {
-    char ten[10];
-    int maMon;
-    char tenMon[20];
-    float diem;
-    cout << "Nhập tên: ";
-    cin >> ten;
-    cout << "Nhập tên môn học: ";
-    cin >> tenMon;
-
-    cout << "Nhập mã môn học và điểm thi\ntheo định dạng aaa-x.x: ";
-    scanf("%d-%f", &maMon, &diem);
-    printf("%s %03d %s %.1f", ten, maMon, tenMon, diem);
-
     return 0;
 }
