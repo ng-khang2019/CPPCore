1c08651b7b7c4093d54901bbddb3a7bed1fcd340
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-24 11:23:22 +0700

    Add two more methods using inner if statements; and ternary operator

--- L06_ConditionalStatements/IfElse.cpp
+++ L06_ConditionalStatements/IfElse.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
-int findMaxBetweenThree (int a, int b, int c);
+int findMaxBetweenThree1 (int a, int b, int c);
 int findMaxBetweenThree2 (int a, int b, int c);
+int findMaxBetweenThree3 (int a, int b, int c);
+int findMaxBetweenThree4 (int a, int b, int c);
 int main() {
     int a,b,c;
     std::cout<<"Enter first number: "; std::cin>>a;
@@ -10,16 +12,35 @@ int main() {
     std::cout<<"Largest number is " << findMaxBetweenThree2(a,b,c) << endl;
     return 0;
 }
-int findMaxBetweenThree (int a, int b, int c) {
+int findMaxBetweenThree1 (int a, int b, int c) {
     int m = 0;
-    if (a > b && a > c) m = a;
-    if (b > c && b > a) m = b;
-    if (c > a && c > b) m = c;
+    if (a >= b && a >= c) m = a;
+    else if (b >= c && b >= a) m = b;
+    else if (c >= a && c >= b) m = c;
     return m;
 }
-int findMaxBetweenThree2 (int a, int b, int c) {
+
+int findMaxBetweenThree2 (int a, int b, int c)
+{
+    int m = 0;
+    if (a >= b) {
+        if (a >= c) m = a;
+        else m = c;
+    } else {
+        if (b >= c) m = b;
+        else m = c;
+    }
+}
+
+int findMaxBetweenThree3 (int a, int b, int c) {
     int m = a;
     if (m < b) m = b;
     if (m < c) m = c;
+    return m;
+}
+
+int findMaxBetweenThree4 (int a, int b, int c)
+{
+    int m = (a >= b) ? (a >= c ? a : c ) : (b >= c ? b : c);
     return m;
 }
