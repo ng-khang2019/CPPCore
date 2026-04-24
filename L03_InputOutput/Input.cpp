78cccb5a16f40c0b60614ab78bfce8569ed1cbc1
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-24 10:07:23 +0700

    -Some issues with scanf(), fgets() -Use getline() and cin for best practices

--- L03_InputOutput/Input.cpp
+++ L03_InputOutput/Input.cpp
@@ -11,15 +11,17 @@ int main()
     // Note that scanf() can only be used with char array
     string id;
     char phoneNumber[15];
-    char unitCode[4];
-    char roomCode[5];
+    char unitCode[10];
+    char roomCode[10];
 
     cout<<"Enter student ID: ";
     cin>>id;
-    cout<<"Enter phone Number: ";
+    cout<<"Enter phone number: ";
     scanf("%s", &phoneNumber);
-    cout<<"Enter unit code and room code following this format ABC-XXXX: ";
-    scanf("%s-%s", &unitCode,&roomCode);
+    cout<<"Enter unit code and room code following this format ABC - XXXX: ";
+    // Pay attention: scanf() will read all the input characters until a white space character appears
+    // Do this will cause characters missing in the next variable scanf("%s-%s",&unitCode,&roomCode)
+    scanf("%s - %s", &unitCode,&roomCode);
 
     // getline(cin,variable) is used to input space included string (mutiples words)
     // Remember to use cin.ignore() to skip the "\n" after the usage scanf() or cin otherwise
@@ -31,7 +33,7 @@ int main()
 
     // cin.getline(variable,size) is used to specify the length of input string
     // Note that getline() and cin.getline() only work with String class
-    cout<<"Enter your tutor Name: ";
+    cout<<"Enter your tutor name: ";
     char tutorName[50];
     cin.getline(tutorName,40);
 
@@ -40,7 +42,14 @@ int main()
     cout<<"Enter your campus area: ";
     char campus[30];
     fgets(campus,30,stdin);
+    campus[strlen(campus)-1]='\0';
 
+    // Use get() to input a single character
+    cout<<"Enter your best grade (A B C D): ";
+    char bestGrade;
+    bestGrade = cin.get();
+
+    cout << "\n--- STUDENT INFO ---" << endl;
     cout<<"Student ID: "<<id<<endl;
     cout<<"Phone number: "<<phoneNumber<<endl;
     cout<<"Unit code: "<<unitCode<<endl;
@@ -48,6 +57,13 @@ int main()
     cout<<"Full name: "<<myName<<endl;
     cout<<"Tutor name: "<<tutorName<<endl;
     cout<<"Campus: "<<campus<<endl;
+    cout<<"Best grade: "<<bestGrade<<endl;
+
+    /* Conclusion: Best practices recommended by senior programmers are
+     * to use string class for string variables declaration, use "cin" for
+     * single words input, "getline()" and "cin.getline()" for strings containing
+     * space characters (multiple words).
+     */
 
     return 0;
 }
