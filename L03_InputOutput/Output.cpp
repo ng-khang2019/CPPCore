ce854cd341d05d3825c855a55cb92fadeaab2572
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-04-24 10:55:43 +0700

    Output in C++

--- L03_InputOutput/Output.cpp
+++ L03_InputOutput/Output.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
-#include <string.h>
 #include <stdio.h>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
-    string id;string myName;
-    cout<<"Nhập MSSV: ";
-    cin>>id;
-    cin.ignore();
-    cout<<"Nhập tên đầy đủ: ";
-    getline(cin, myName);
+    /* Use most common ways to print output is to use "cout" or "printf()".
+     * Use "cout" for quick and standard output, "printf()" for formatted
+     * output.
+     */
+    cout<<"Common output methods: "<<endl;
+    int a = 4; char b = 'b'; float c = 3.14234;
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    printf("%d\t%c\t%.2f\n",a,b,c);
+    cout<<"\nAdvanced formatted output from <iomanip> library: "<<endl;
+    cout<<setw(15)<<left<<"ID";
+    cout<<setw(20)<<left<<"Name";
+    cout<<setw(30)<<right<<"Major"<<endl;
 
-    char lecturer1[30], lecturer2[30], lecturer3[30];
-    cout<<"Nhập tên giảng viên môn Lập Trình: ";
-    cin.getline(lecturer1,29);
-    cout<<"Nhập tên giảng viên môn Giải Tích: ";
-    fgets(lecturer2,29,stdin); // fgets is SAFE and Recommended
-    cout<<"Nhập tên giảng viên môn Đại Số: ";
-    fgets(lecturer3,29,stdin);
+    cout<<setfill('-');
+    cout<<setw(65)<<""<<endl;
+    cout<<setfill(' ');
 
-    cout<<"Tên: "<<myName<<endl;
-    cout<<"MSSV: "<<id<<endl;
-    cout<<"GV Lập Trình: "<<lecturer1<<endl;
-    cout<<"GV Giải Tích: "<<lecturer2<<endl;
-    cout<<"GV Đại Số : "<<lecturer3<<endl;
-    cin.get();
+    cout<<setw(15)<<left<<60020734;
+    cout<<setw(20)<<left<<"Khang Nguyen";
+    cout<<setw(30)<<right<<"Information Security"<<endl;
     return 0;
 }
-
-/*
-     * Using getline() right after cin will create a bug since getline
-     * reads the Enter character '\n' in the buffer cache, from cin.
-     * To ignore '\n' from the above cin command, use cin.ignore().
-     * This will skip '\n' character.
-     */
