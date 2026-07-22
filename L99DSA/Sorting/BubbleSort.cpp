df146552f3c42d83c573fc03f9754ec1263c6255
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-07-22 15:13:33 +0700

    refactor: revise BubbleSort to improve loop logic and integrate std::size for array handling

--- L99DSA/Sorting/BubbleSort.cpp
+++ L99DSA/Sorting/BubbleSort.cpp
@@ -4,11 +4,11 @@ using namespace std;
 void bubbleSort(int arr[], int n)
 {
     bool swapped = false;
-    for (int i = 0; i < n;i++)
+    for (int i = 0; i < n-1;i++)
     {
-        for (int j = 0; j < n-i-1;j++)
+        for (int j = n-1; j > i;j--)
         {
-            if (arr[j+1] < arr[j]) swap(arr[j], arr[j+1]);
+            if (arr[j-1] > arr[j]) swap(arr[j], arr[j-1]);
             swapped = true;
         }
         if (!swapped) break;
@@ -27,7 +27,8 @@ void printArray(int arr[], int n)
 int main()
 {
     int arr[] = {7,4,8,9,5,3,5,7,5,2,4,5};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = std::size(arr);
+    printArray(arr, n);
     bubbleSort(arr, n);
     printArray(arr, n);
     return 0;
