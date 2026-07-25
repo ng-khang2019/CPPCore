f25eee7af137dabe6a39f17c62cae3ceec738dc6
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-07-25 15:05:50 +0700

    refactor: clean up code formatting in InsertionSort.cpp and simplify loop logic

--- L99DSA/Sorting/04InsertionSort.cpp
+++ L99DSA/Sorting/04InsertionSort.cpp
@@ -3,20 +3,17 @@ using namespace std;
 
 void insertionSort(int arr[], int n)
 {
-    for (int i = 1; i < n;i++)
+    for (int i = 1; i < n; i++)
     {
         int temp = arr[i];
-        int j = i -1;
-        if (temp < arr[j])
+        int j = i - 1;
+        while (j >= 0 && temp < arr[j])
         {
-            while (j >= 0 && temp < arr[j])
-            {
-                // Shift
-                arr[j+1] = arr[j];
-                j--;
-            }
+            // Shift
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[j+1] = temp;
+        arr[j + 1] = temp;
     }
 }
 
@@ -31,7 +28,7 @@ void printArray(int arr[], int n)
 
 int main()
 {
-    int arr[] = {7,4,8,9,5,3,5,7,5,2,4,5};
+    int arr[] = {7, 4, 8, 9, 5, 3, 5, 7, 5, 2, 4, 5};
     int n = std::size(arr);
     printArray(arr, n);
     insertionSort(arr, n);
