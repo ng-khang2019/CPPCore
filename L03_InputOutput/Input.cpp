7d2e80ba5e0a65ab5f86baddd52545494c247ea5
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-08-03 17:46:01 +0700

    feat: add cin.get() function and edit some comments

--- L03_InputOutput/Input.cpp
+++ L03_InputOutput/Input.cpp
@@ -9,37 +9,53 @@ int main()
     // cin and scanf() are used for inputting non-space string (single words)
     // Use scanf() when you need to input string followed a format
     // Note that scanf() can only be used with char array
-    string id;
-    char phoneNumber[15];
-    char unitCode[10];
-    char roomCode[10];
-
     cout<<"Enter student ID: ";
+    string id;
     cin>>id;
     cout<<"Enter phone number: ";
+    char phoneNumber[15];
     scanf("%s", &phoneNumber);
+
+    /* Pay attention: scanf() will read all the input characters until a white space character appears
+     * Do this will cause characters missing in the next variable scanf("%s-%s",&unitCode,&roomCode)
+     */
     cout<<"Enter unit code and room code following this format ABC - XXXX: ";
-    // Pay attention: scanf() will read all the input characters until a white space character appears
-    // Do this will cause characters missing in the next variable scanf("%s-%s",&unitCode,&roomCode)
+    char unitCode[10];
+    char roomCode[10];
     scanf("%s - %s", &unitCode,&roomCode);
 
-    // getline(cin,variable) is used to input space included string (mutiples words)
-    /*
+    /* getline(cin,variable) is used to input space included string (mutiples words)
      * Using getline() right after cin will create a bug since getline
      * reads the Enter character '\n' in the buffer cache, from cin.
      * To ignore '\n' from the above cin command, use cin.ignore().
      * This will skip '\n' character.
      */
-    string myName;
     cin.ignore();
+    string myName;
     cout<<"Enter your full name: ";
     getline(cin, myName);
 
-    // cin.getline(variable,size) is used to specify the length of input string
-    // Note that getline() only works with String class, while cin.getline() works with char type
+    /* cin.getline(variable,size) is used to specify the length of input string
+     * Note that getline() only works with String class, while cin.getline() works with char type
+     */
     cout<<"Enter your tutor name: ";
     char tutorName[50];
     cin.getline(tutorName,40);
+
+    /* cin.get() is very similar to cin.getline() however cin.get() keeps the
+     * '\n' in the input stream, therefore, you must use cin.ignore() to "consome"
+     * the '\n' character
+     */
+    cout<<"Enter your favorite subject: ";
+    char favoriteSubject[20];
+    cin.get(favoriteSubject,20,'\n');
+    cin.ignore();
+
+    // Use get() to input a single character
+    cout<<"Enter your best grade (A B C D): ";
+    char bestGrade;
+    bestGrade = cin.get();
+    cin.ignore();
 
     // fgets(variable,size,stdin) is recommended for its type safety and size check
     // gets() was commonly used before but is now deprecated due to its unsafe memory allocation
@@ -48,11 +64,6 @@ int main()
     fgets(campus,30,stdin);
     campus[strlen(campus)-1]='\0';
 
-    // Use get() to input a single character
-    cout<<"Enter your best grade (A B C D): ";
-    char bestGrade;
-    bestGrade = cin.get();
-
     cout << "\n--- STUDENT INFO ---" << endl;
     cout<<"Student ID: "<<id<<endl;
     cout<<"Phone number: "<<phoneNumber<<endl;
@@ -60,8 +71,9 @@ int main()
     cout<<"Room code: "<<roomCode<<endl;
     cout<<"Full name: "<<myName<<endl;
     cout<<"Tutor name: "<<tutorName<<endl;
-    cout<<"Campus: "<<campus<<endl;
+    cout<<"Favorite subject: "<<favoriteSubject<<endl;
     cout<<"Best grade: "<<bestGrade<<endl;
+    cout<<"Campus: "<<campus<<endl;
 
     /* Conclusion: Best practices recommended by senior programmers are
      * to use string class for string variables declaration, use "cin" for
