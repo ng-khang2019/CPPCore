c7393f8550538e312cc33d40bf409e8b8df61e66
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-08-16 16:35:52 +0700

    refactor: remove unused addNode function and improve head/tail removal logic in SinglyLinkedList01.cpp

--- L99DSA/03SinglyLinkedList/SinglyLinkedList01.cpp
+++ L99DSA/03SinglyLinkedList/SinglyLinkedList01.cpp
@@ -29,21 +29,6 @@ void createLinkedList(List &list)
     list.size = 0;
 }
 
-void addNode(LinkedList &list, int value)
-{
-    Node* newNode = createNode(value);
-    if (list.pHead == nullptr)
-    {
-        list.pHead = newNode;
-        list.pTail = newNode;
-    } else
-    {
-        list.pTail -> next = newNode;
-        list.pTail = newNode;
-    }
-    list.size++;
-}
-
 void addHead(LinkedList &list, int value)
 {
     Node* newNode = createNode(value);
@@ -76,12 +61,48 @@ void addTail(LinkedList &list, int value)
 
 void removeHead(LinkedList &list)
 {
+    if (list.pHead != nullptr)
+    {
+        Node* oldHead = list.pHead;
+        if (list.pHead == list.pTail)
+        {
+            list.pHead = nullptr;
+            list.pTail = nullptr;
+        } else
+        {
+            Node* newHead = list.pHead -> next;
+            oldHead -> next = nullptr;
+            list.pHead = newHead;
+        }
 
+        delete oldHead;
+        list.size--;
+    }
 }
-
 void removeTail(LinkedList &list)
 {
+    if (list.pTail != nullptr)
+    {
+        Node* oldTail = list.pTail;
+        if (list.pHead == list.pTail)
+        {
+            list.pHead = nullptr;
+            list.pTail = nullptr;
+        } else
+        {
+            Node* prevTail = list.pHead;
+            while (prevTail -> next != list.pTail)
+            {
+                prevTail = prevTail -> next;
+            }
 
+            prevTail -> next = nullptr;
+            list.pTail = prevTail;
+        }
+
+        delete oldTail;
+        list.size--;
+    }
 }
 
 void removeNode(LinkedList &list, int value)
@@ -91,7 +112,12 @@ void removeNode(LinkedList &list, int value)
 
 bool insertNode(LinkedList &list, int value, int index)
 {
-    if (index < 0 || index > list.size) return false;
+
+}
+
+void reverseList(LinkedList &list)
+{
+
 }
 
 
