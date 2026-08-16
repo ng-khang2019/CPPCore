11d4e25e589dfd31ea4b2abb767868d7848c9423
mkn <192448446+ng-khang2019@users.noreply.github.com>
2026-08-16 07:30:02 +0700

    feat: add SinglyLinkedList02.cpp with class definition and basic linked list operations

---
+++ L99DSA/03SinglyLinkedList/SinglyLinkedList02.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int value;
+    Node* next;
+    Node(int value) : value(value), next(nullptr) {}
+};
+
+class LinkedList
+{
+private:
+    Node* pHead;
+    Node* pTail;
+    int size;
+public:
+    LinkedList() : pHead(nullptr), pTail(nullptr), size(0) {}
+
+    ~LinkedList()
+    {
+        Node* current = pHead;
+        while (current != nullptr)
+        {
+            Node* next = temp -> next;
+            delete current;
+            current = next;
+        }
+        pHead = nullptr;
+    }
+
+
+    void addHead(int value);
+    void addTail(int value);
+    void insertNode(int value, int index);
+
+    Node* getNode(int index);
+    void setNode(int index, int value);
+
+    void removeHead();
+    void removeTail();
+    void removeNode(int value);
+
+    void print();
+    void reverse();
+};
